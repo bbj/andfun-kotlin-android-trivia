04a338dc8b36fcb39bafca7a03953d21b8b1b9eb
Bruno Jullien <bruno.jullien@gmail.com>
2022-11-11 12:14:47 -0800

    9. Exercise: Let’s Navigate Already

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -19,9 +21,23 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_title, container, false
-        )
+            inflater, R.layout.fragment_title, container, false)
+
+//        // Kotlin anonymous function: val sum = { a: Int, b: Int -> a + b } assigned as a literal!
+//        binding.playButton.setOnClickListener { view: View ->
+//            Navigation.findNavController(view).navigate(
+//                R.id.action_titleFragment_to_gameFragment //generated into navigation.xml
+//            )
+//            //optimization 1: version using kotlin extensions functions
+//            //view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment);
+//        }
+
+        //optimization 2: Navigation can create the listener for you!
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+
         return binding.root // the root of the layout we just inflated
     }
 }
