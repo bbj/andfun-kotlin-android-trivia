758c6679218adff27247b25829cedd1da93bf87a
Bruno Jullien <bruno.jullien@gmail.com>
2022-12-01 00:58:36 +0100

    27. Exercise: Using Navigation Listeners

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,27 +19,54 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout         //lateinit: allow init a non-null prop outside constructor
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        drawerLayout = binding.drawerLayout
+
         //Find NavController using KTX extension function
         val navController = this.findNavController(R.id.myNavHostFragment)
         //Link the NavController to the ActionBar with NavigationUI.setupWithNavController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        //Then we need to include the drawerLayout as 3rd param
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
+        // prevent nav gesture if not on start destination
+        // _: when a param is not used, name it "_"
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+        //Let the Navigation UI to know about the navigation view
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     //what happen when UP is pressed
     override fun onSupportNavigateUp(): Boolean {
         //DEFAULT CODE: return super.onSupportNavigateUp()
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        //return navController.navigateUp()
+        //this is so the navigation UI can replace the Up Button with the navigation drawer button
+        // when we get to the start destination
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
